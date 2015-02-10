05f13f578924933695c8540f8aa33fdbda68ace2
soumyas23 <soumy.s@experionglobal.com>
2015-02-10 17:40:37 +0530

    voting app

--- newsapp/public/ui/home/home.js
+++ newsapp/public/ui/home/home.js
@@ -1,4 +1,4 @@
-angular.module('ui').controller('HomeCtrl',function($scope,$firebase){
+angular.module('ui' ).controller('HomeCtrl',function($scope,$firebase,$filter){
 $scope.showadd=true;
 
 $scope.add=function(){
@@ -11,7 +11,11 @@ var fb = $firebase(ref);
 var syncObject = fb.$asObject();
 
   // three way data binding
- 
+
+
+
+
+  
   
 $scope.save=function(news){
   var title=news.item;
@@ -23,7 +27,9 @@ fb.$push({
        description:description,
 		    vote:vote
      });
-$scope.item=false;  
+  // for(var i=0;i<)
+
+  $scope.item=false;  
 $scope.showadd=true;
 };
  syncObject.$bindTo($scope,'newsadded');
@@ -35,13 +41,67 @@ $scope.cancel=function(){
 
 
 };
-$scope.like=function(newses){
+ //$scope.syncObject = $filter('orderBy')($scope.syncObject,'vote' );
+  console.log($scope.syncObject);
+$scope.like=function(newses,newsadded){
  newses.vote++;
+ var newsData=[];
+  var k = 0 ;
+ for(var key in newsadded){
+    newsData[k]  = newsadded[key];
+         k++;                                
+    }
+console.log(newsData);
+for(var i = 2 ;i<newsData.length;i++){
+  for(var j = 2;j<newsData.length - 1;j++){
+   if(newsData[j].vote < newsData[j+1].vote){
+    var t = newsData[j];
+    newsData[j] = newsData[j+1];
+    newsData[j+1] = t;
+     }
+  }
+ }
+    i = 0 ;
+   console.log(newsData);
+   for(var keys in newsadded){
+        newsadded[keys]  = newsData[i];
+         i++;                                
+    }
+    console.log(newsadded);
+    console.log(newsData);
+
+
 };
 
-$scope.dislike=function(newses){
- newses.vote--;
+$scope.dislike=function(newses,newsadded){
+       newses.vote--;
+      var newsData=[];
+      var k = 0 ;
+      for(var key in newsadded){
+         newsData[k]  = newsadded[key];
+         k++;                                
+       }
+        console.log(newsData);
+  for(var i = 2 ;i<newsData.length;i++){
+    for(var j = 2;j<newsData.length - 1;j++){
+      if(newsData[j].vote < newsData[j+1].vote){
+       var t = newsData[j];
+        newsData[j] = newsData[j+1];
+       newsData[j+1] = t;
+     }
+  }
+ }
+    i = 0 ;
+   console.log(newsData);
+   for(var keys in newsadded){
+        newsadded[keys]  = newsData[i];
+         i++;                                
+    }
+    console.log(newsadded);
+    console.log(newsData);
+
 };
+
 
 
 
